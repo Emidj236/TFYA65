d6e63a988c3970e78360a9f790155373f0cd1cad
Emidj236 <emidj236@student.liu.se>
2024-09-19 19:28:15 +0200

    Blabla

--- Assets/VisualizeWav.cs
+++ Assets/VisualizeWav.cs
@@ -5,12 +5,19 @@ using Seb.Vis;
 
 public class VisualizeWav : MonoBehaviour
 {
+    [Header("Wave file")]
+    [SerializeField]
+    bool showAudioFile;
+    [SerializeField]
+    AudioClip audioFile;
+
     [Header("Waves")]
     [SerializeField]
     bool sinusWave = true;
     [SerializeField]
     bool cosinusWave;
-
+    [SerializeField]
+    float waveLength;
     [Space(5)]
 
     [Header("Point Settings")]
@@ -21,21 +28,39 @@ public class VisualizeWav : MonoBehaviour
     [SerializeField]
     Color pointColor = Color.white;
     [SerializeField]
-    int startPointLeft;
-    [SerializeField]
     float amplitude;
-
-    Vector2[] positions;
-
-    void Start()
-    {
-        positions = new Vector2[Mathf.FloorToInt(length / pointRadius)];
-    }
 
     void Update()
     {
         Draw.StartLayer(Vector2.zero, 1f, false);
 
-        Draw.Point(Vector2.zero, pointRadius, pointColor);
+        if (!showAudioFile)
+        {
+            for (int i = 0; i < Mathf.FloorToInt(length / pointRadius); i++)
+            {
+                if (sinusWave)
+                {
+                    Draw.Point(new Vector2((i * pointRadius) - (length / 2.0f), SinusWave(ref i)), pointRadius, pointColor);
+                }
+                else if (cosinusWave)
+                {
+                    Draw.Point(new Vector2((i * pointRadius) - (length / 2.0f), CosSinusWave(ref i)), pointRadius, pointColor);
+                }
+            }
+        } else if (showAudioFile)
+        {
+            Debug.Log(audioFile.name);
+        }
+    }
+
+    // Pass by refrence sinus våg
+    float SinusWave(ref int _index)
+    {
+        return Mathf.Sin(_index * waveLength * Mathf.PI * 2) * amplitude;
+    }
+
+    float CosSinusWave(ref int _index)
+    {
+        return Mathf.Cos(_index * waveLength * Mathf.PI * 2) * amplitude;
     }
 }
